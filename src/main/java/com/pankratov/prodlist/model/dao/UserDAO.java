e4bc93ede64de6bee497d7ea4f7b7fb7a049e156
myFirstSteps <michaelleo@mail.ru>
2014-12-10 23:12:22 +0300

    Update

--- src/main/java/com/pankratov/prodlist/model/dao/UserDAO.java
+++ src/main/java/com/pankratov/prodlist/model/dao/UserDAO.java
@@ -1,18 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.pankratov.prodlist.model.dao;
 import com.pankratov.prodlist.model.dao.jdbc.JDBCDAOException;
 import com.pankratov.prodlist.model.users.User;
 import java.util.concurrent.*;
 
-/**
- *
- * @author pankratov
- */
 public interface UserDAO extends AutoCloseable {
     public  User registerUser(User user) throws Exception;
     public User readUser(String name)throws Exception;
